ac45909c14e031b1c042f0f225b5f8c42a734bf5
roxsyVq <roxsy.dj@gmail.com>
2024-02-12 16:40:16 -0500

    animacion en js

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+const navLink = document.querySelectorAll(".nav__link");
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((item) => item.addEventListener("click", linkAction));
